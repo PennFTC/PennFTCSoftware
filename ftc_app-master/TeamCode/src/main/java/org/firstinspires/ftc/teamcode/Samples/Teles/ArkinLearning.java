ea8c1eda07a4f0807c34a50454fb3e6b8db3feaf
ArkinK <arkink2002@gmail.com>
2018-03-15 17:27:56 -0400

    Test

---
+++ ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Samples/Teles/ArkinLearning.java
@@ -0,0 +1,24 @@
+package org.firstinspires.ftc.teamcode.Samples.Teles;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+/**
+ * Created by admin2 on 3/15/2018.
+ */
+@TeleOp
+@Disabled
+public class ArkinLearning extends OpMode {
+    public DcMotor leftdrive, rightdrive;
+
+    public void start()
+    }
+
+    public void init ()
+        leftdrive = hardwareMap.DcMotor.get ("leftdrive");
+
+
+}
