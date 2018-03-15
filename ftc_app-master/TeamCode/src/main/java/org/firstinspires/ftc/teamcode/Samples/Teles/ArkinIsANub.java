a1acd85a5ee57189ce5eccdb99dde8825f3a6751
ArkinK <arkink2002@gmail.com>
2018-03-15 16:45:42 -0400

    Arkin nub

---
+++ ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Samples/Teles/ArkinIsANub.java
@@ -0,0 +1,55 @@
+package org.firstinspires.ftc.teamcode.Samples.Teles;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+import static java.lang.Math.PI;
+
+/**
+ * Created by admin2 on 3/15/2018.
+ */
+
+public class ArkinIsANub {package org.firstinspires.ftc.teamcode.Samples.Teles;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+import static java.lang.Math.PI;
+
+    @TeleOp
+    public class ArkinIsANub extends OpMode {
+        DcMotor leftfront, rightfront, leftback, rightback;
+
+        public void init() {
+            leftfront = hardwareMap.dcMotor.get("leftfront");
+            rightfront = hardwareMap.dcMotor.get("rightfront");
+            leftback = hardwareMap.dcMotor.get("leftback");
+            rightback = hardwareMap.dcMotor.get("rightback");
+            rightfront.setDirection(DcMotorSimple.Direction.REVERSE);
+            rightback.setDirection(DcMotorSimple.Direction.REVERSE);
+            leftfront.setPower(0);
+            rightfront.setPower(0);
+            leftback.setPower(0);
+            rightback.setPower(0);
+        }
+
+        public void loop() {
+            double r = Math.hypot(gamepad1.left_stick_x, gamepad1.left_stick_y);
+            double robotAngle = Math.atan2(gamepad1.left_stick_y, -gamepad1.left_stick_x) - PI / 4;
+            double rightX = -(.25 *(gamepad1.right_stick_x));
+            final double v1 = r * Math.cos(robotAngle) + rightX;
+            final double v2 = r * Math.sin(robotAngle) - rightX;
+            final double v3 = r * Math.sin(robotAngle) + rightX;
+            final double v4 = r * Math.cos(robotAngle) - rightX;
+            leftfront.setPower(v1);
+            rightfront.setPower(v2);
+            leftback.setPower(v3);
+            rightback.setPower(v4);
+        }
+    }
+
+}
